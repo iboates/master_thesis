cd752f3c62b05da7ebbd1408b8d4336bdd298a95
Isaac Boates <boates@eif-term-lin-01.eifer.kit.edu>
2018-04-10 10:32:52 +0200

    MeasurementDevices added to import code

--- import_3dcitydb/nanaimo_to_3dcitydb.py
+++ import_3dcitydb/nanaimo_to_3dcitydb.py
@@ -5,7 +5,8 @@ from src.UtilityNetworkADE2DB import import_Network
 
 if __name__ == "__main__":
 
-    conn = psycopg2.connect("host=localhost dbname=unade user=postgres password=postgres")
+#     conn = psycopg2.connect("host=localhost dbname=unade user=postgres password=postgres")
+    conn = psycopg2.connect("host=eif-pg-96-01 dbname=isaac_thesis user=isaac_thesis_serviceaccount password=z23jIfZXKaoUrG8F")
 
     # conn.autocommit = True
 
@@ -13,7 +14,7 @@ if __name__ == "__main__":
 
     # citygml = ET.parse('/media/isaac/Data/_GMCM/Thesis/data/citygml/nanaimo_water_network.gml')
     # citygml = ET.parse('/media/isaac/Data/_GMCM/Thesis/data/citygml/nanaimo_electric_network.gml')
-    citygml = ET.parse('/media/isaac/Data/_GMCM/Thesis/data/citygml/nanaimo_water_and_electric_network.gml')
+    citygml = ET.parse('../data/citygml/nanaimo_water_and_electric_network.gml')
 
     import_Network(citygml, cur)
 
